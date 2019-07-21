53ed12b1235f4c9a3db41f86d3f1dcbbf1265d19
ciscoff <ciscoff@ya.ru>
2019-07-21 19:47:18 +0300

    Project with minimum code. Load maps only

---
+++ app/src/main/java/k/s/yarlykov/travelmeteo/MainActivity.kt
@@ -0,0 +1,32 @@
+package k.s.yarlykov.travelmeteo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import k.s.yarlykov.travelmeteo.ui.MapActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnShowMap.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btnShowMap -> {
+                MapActivity.start(this, "Some data")
+
+            }
+
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/k/s/yarlykov/travelmeteo/ui/MapActivity.kt
@@ -0,0 +1,45 @@
+package k.s.yarlykov.travelmeteo.ui
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import k.s.yarlykov.travelmeteo.R
+
+class MapActivity : FragmentActivity(), OnMapReadyCallback {
+
+    companion object {
+        private val EXTRA_MAP = MapActivity::class.java.simpleName + "exrta.MAP"
+
+        fun start(context: Context?, extraData: String) {
+            val intent = Intent(context, MapActivity::class.java).apply {
+                putExtra(EXTRA_MAP, extraData)
+            }
+            context?.startActivity(intent)
+        }
+    }
+
+    lateinit var googleMap:GoogleMap
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        googleMap =  map!!
+    }
+
+
+
+
+}
