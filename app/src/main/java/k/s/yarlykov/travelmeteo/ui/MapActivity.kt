70f7a6962a04f9ca698808feae7f24ad676990f7
ciscoff <ciscoff@ya.ru>
2019-07-21 21:12:23 +0300

    Options menu created.

--- app/src/main/java/k/s/yarlykov/travelmeteo/ui/MapActivity.kt
+++ app/src/main/java/k/s/yarlykov/travelmeteo/ui/MapActivity.kt
@@ -2,18 +2,20 @@ package k.s.yarlykov.travelmeteo.ui
 
 import android.content.Context
 import android.content.Intent
+import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import k.s.yarlykov.travelmeteo.R
 
-class MapActivity : FragmentActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     companion object {
-        private val EXTRA_MAP = MapActivity::class.java.simpleName + "exrta.MAP"
+        private val EXTRA_MAP = MapActivity::class.java.simpleName + "extra.MAP"
 
         fun start(context: Context?, extraData: String) {
             val intent = Intent(context, MapActivity::class.java).apply {
@@ -23,23 +25,33 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
         }
     }
 
-    lateinit var googleMap:GoogleMap
+    private var googleMap: GoogleMap? = null
+
+    lateinit var locationManager: LocationManager
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
     }
 
-    override fun onMapReady(map: GoogleMap?) {
-        googleMap =  map!!
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_map, menu)
+        return true
     }
 
+    override fun onMapReady(map: GoogleMap?) {
+        googleMap = map
+        googleMap?.let {
+            it.uiSettings.isZoomControlsEnabled = true
 
+        }
+    }
 
 
 }
